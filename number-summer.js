e06cb9dd79710e6a0f5048f96ccad3a1ee6686b9
Kee <shikee.maloney@codeimmersives.com>
2022-09-23 14:36:41 -0400

    update

--- number-summer.js
+++ number-summer.js
@@ -5,13 +5,13 @@ let someNumbers = Number(prompt(`Enter some numbers, then enter '0' when complet
 
 
 total = 0
-
+final = 0
 while (someNumbers > 0){
  
  someNumbers = Number(prompt("> "))
 
  total += someNumbers
-
+final +=total
 }
 
-console.log(total)
+console.log(final)
